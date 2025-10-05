7dd4c036bbcb8de84888f516746760c6ad7f3943
yuasalily <yuasalily1011@gmail.com>
2025-10-05 08:53:40 +0000

    API(Port)を定義

---
+++ internal/app/ports.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"context"
+	"time"
+
+	"github.com/yuasalily/harbor-tui/internal/docker"
+)
+
+type DockerAPI interface {
+	Info(ctx context.Context) (docker.DaemonInfo, error)
+	ImagesList(ctx context.Context, opts ImagesListOptions) ([]ImageSummary, error)
+}
+
+type DaemonInfo struct {
+	Version    string
+	OS         string
+	APIVersion string
+}
+
+type ImageSummary struct {
+	ID        string
+	RepoTags  []string
+	Size      int64
+	CreatedAt time.Time
+}
+
+type ImagesListOptions struct {
+	All       bool  //中間イメージを含む
+	Dangling  *bool // nil=未指定, true/falseをfiltersに渡す
+	Reference string
+}
